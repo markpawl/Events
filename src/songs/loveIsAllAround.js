7689d8555f450a93ec85d6abe413ec6276786635
= <mark.pawlowski@webagesolutions.com>
2024-05-05 12:45:41 +0200

    update lyrics

--- src/songs/loveIsAllAround.js
+++ src/songs/loveIsAllAround.js
@@ -1,11 +1,22 @@
 let song = {
-"title": "* Love Is All Around",
+"title": "Love Is All Around",
 "lyrics-en":
-`
-Summer days, down at the beach
-Looking for someone that's looking for me
+`Summer days, down at the beach
+Wishing that someone was out there for me
 Kites in the sky, look at them go
-They're up in the air, and we're down below
+Up in the air, and we're down below
+
+Stumbling around, who would believe
+We'd meet each other - you and me
+Having fun, as much as allowed
+Heads in the sky, and feet on the ground
+
+We Searched all around the world,
+Asked every boy and every girl
+In every street and every town,
+This is what we found...
+
+Love is all around
 
 A gentle breeze, toes in the sand
 you and I walking hand in hand
@@ -13,34 +24,30 @@ skipping stones into the sea
 I'm so happy when you're with me
 
 We Searched all around the world,
-Asked every boy and every girl
-In every street and every town,
-This is what we found...
-Love is all around
-
-Our love is all around
-Our love is all around
-Our love is all around
-Our love is all around
-
-Walking together, who would believe
-We've got each other, you and me
-everyday people, just hanging around
-Heads in the sky and feets on the ground
-
-We Searched all around the world,
 Met every boy and every girl
 On every street in every town,
 This is what we found...
+
 Love is all around
 `,
 
 "lyrics-de":
-`
-Sommertage, unten am Strand
-Ich suche jemanden, der mich sucht
+`Sommertage, unten am Strand
+Ich wünschte, dass jemand für mich da wäre
 Drachen am Himmel, sieh sie dir an
-Sie sind in der Luft und wir sind unten
+Oben in der Luft und wir sind unten
+
+Herumstolpern, wer würde das glauben
+Wir würden uns treffen – du und ich
+Spaß haben, soweit erlaubt
+Köpfe in den Himmel und Füße auf dem Boden
+
+Wir haben auf der ganzen Welt gesucht,
+Fragte jeden Jungen und jedes Mädchen
+In jeder Straße und jeder Stadt,
+Das haben wir gefunden...
+
+Liebe ist überall
 
 Eine sanfte Brise, die Zehen im Sand
 Du und ich gehen Hand in Hand
@@ -48,25 +55,10 @@ Steine ins Meer werfen
 Ich bin so glücklich, wenn du bei mir bist
 
 Wir haben auf der ganzen Welt gesucht,
-Fragte jeden Jungen und jedes Mädchen
-In jeder Straße und jeder Stadt,
-Das haben wir gefunden...
-Liebe ist überall
-
-Unsere Liebe ist überall
-Unsere Liebe ist überall
-Unsere Liebe ist überall
-Unsere Liebe ist überall
-
-Gemeinsam spazieren gehen, wer würde das glauben
-Wir haben uns, du und ich
-ganz normale Menschen, die einfach nur herumhängen
-Köpfe in den Himmel und Füße auf der Erde
-
-Wir haben auf der ganzen Welt gesucht,
 Habe jeden Jungen und jedes Mädchen getroffen
 Auf jeder Straße in jeder Stadt,
 Das haben wir gefunden...
+
 Liebe ist überall
 `
 }
